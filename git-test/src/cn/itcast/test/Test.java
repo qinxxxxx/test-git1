bfae7753f30df781e312f0176c1d69f828d87a34
wgr <648848371@qq.com>
2019-06-12 19:39:45 +0800

    提交到git

---
+++ git-test/src/cn/itcast/test/Test.java
@@ -0,0 +1,10 @@
+package cn.itcast.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+    }
+}
